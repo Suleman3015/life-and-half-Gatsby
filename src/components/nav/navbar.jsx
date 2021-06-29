a7665f7200a834064279cfafc103bd1c943825e2
Suleman Ahmed <zayn84041@gmail.com>
2021-06-29 13:44:45 +0500

    hero updates

--- src/components/nav/navbar.jsx
+++ src/components/nav/navbar.jsx
@@ -1,37 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "gatsby"
 import '../../styles/nav/nav.css'
 import MenuIcon from '@material-ui/icons/Menu';
+import Hamburger from './hamburger'
 
 
 const Navbar = () => {
-
+    const [show, setShow] = useState(true)
+    const HamShow = () => {
+        setShow(!show)
+    }
     return (
-        <div className="Navbar" >
-            <div className="logo" >
-                <h1>Life And Half</h1>
-            </div>
-            <div className="menu" >
-                <ul>
-                    <Link> Customer Stories </Link>
-                    <Link> About Us</Link>
-
-                </ul>
-                <div className="butDiv">
-                    <button className="but" >GET STARTED</button>
-
-
-
+        <>
+            <div className="Navbar" >
+                <div className="logo" >
+                    {!show ? null :
+                        <h1>Life And Half</h1>}
                 </div>
-                <div className="menuIcon" >
-                    <MenuIcon />
+                <div className="menu" >
+                    <ul>
+                        <Link> Customer Stories </Link>
+                        <Link> About Us</Link>
+
+                    </ul>
+                    <div className="butDiv">
+                        {!show ? null :
+                            <button className="but" >GET STARTED</button>}
+
+
+
+                    </div>
+                    {!show ? null :
+                        <div onClick={HamShow} className="menuIcon" >
+                            <MenuIcon />
+                        </div>}
                 </div>
+
+                {!show && <Hamburger clickFunc={HamShow} />}
+
+
             </div>
 
 
-        </div>
-
-
+        </>
     )
 }
 
